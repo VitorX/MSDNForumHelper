e2ef4353bc4e9be97d68d09efdd31b608b9fa8da
Fei Xue <fxnotepad@gmail.com>
2016-08-11 16:19:52 +0800

    Test the EntityFrameworkd

--- ForumHelperTest/Program.cs
+++ ForumHelperTest/Program.cs
@@ -13,16 +13,31 @@ namespace ForumHelperTest
         {
             var retchRequest = new FetchRequest();
             retchRequest.ForumName = "vsto";
-            var threads= retchRequest.Run();
-            const int nameWidth = -10;
+            retchRequest.PagesPerRequest = 2;
+
+            var threads = retchRequest.Run();
+
+            const int nameWidth = -20;
+            int index = 1;
             foreach (var thread in threads)
             {
+                Console.WriteLine(index++);
                 Console.WriteLine($"{"id",nameWidth}: {thread.ThreadId}");
                 Console.WriteLine($"{"title",nameWidth}: {thread.ThreadTitle}");
                 Console.WriteLine($"{"isAnswered",nameWidth}: {thread.IsAnswered}");
                 Console.WriteLine($"{"createdTime",nameWidth}: {thread.CreatedTime}");
+                Console.WriteLine($"{"lastRepliedBy",nameWidth}: {thread.LastRepliedBy}");
+                Console.WriteLine($"{"lastRepliedTime",nameWidth}: {thread.LastRepliedTime}");
                 Console.WriteLine("");
             }
+
+            ThreadContext threadsContext = new ThreadContext();
+            var threadsFromDB = threadsContext.Threads.ToList();
+            IEnumerable<Thread> newThreads = threads.Except(threadsFromDB);
+            if (newThreads.Count() > 0)
+                threadsContext.Threads.AddRange(newThreads);
+
+            threadsContext.SaveChanges();
             Console.ReadLine();
         }
     }
